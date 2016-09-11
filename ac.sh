02d6066e2f633af94c24e5dd0d9655dd140cfd53
canhnht <canhnht1709@gmail.com>
2016-09-11 12:32:31 +0800

    Add option for submitting

--- ac.sh
+++ ac.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 g++ run-test.cpp && ./a.out $1 $2
 
-xdotool type 'uva' \
-&& xdotool key Return \
-&& xdotool type 'use uva canhnhtse03860' \
-&& xdotool key Return \
-&& xdotool type "send $1 $1.cpp" \
-&& xdotool key Return \
-&& xdotool type 'stat' \
-&& xdotool key Return
+if [ "$3" == "-y" ]; then
+	xdotool type 'uva' \
+	&& xdotool key Return \
+	&& xdotool type 'use uva canhnhtse03860' \
+	&& xdotool key Return \
+	&& xdotool type "send $1 $1.cpp" \
+	&& xdotool key Return \
+	&& xdotool type 'stat' \
+	&& xdotool key Return
+else
+	echo 'Skip submitting'
+fi
